63d778c6495a730ca5530130c7dfee6b623916f2
Chris Blake <chrisrblake93@gmail.com>
2023-08-12 21:04:17 -0500

    feat: first commit, public release
    Enjoy! :)

---
+++ README.md
@@ -0,0 +1,43 @@
+# dopinder-debian
+
+Build script to build a Debian 12 image for the Walmart onn 4k UHD box (android codename dopinder), as well as all dependencies. This includes the following:
+
+- Mainline Linux Kernel - [linux-6.4.y](https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/log/?h=linux-6.4.y)
+- Mainline U-Boot - [v2023.10-rc2](https://github.com/u-boot/u-boot/tree/v2023.10-rc2)
+
+Note that there are patches/modifications applied to the kernel and u-boot. The changes made can be seen in the `./patches` and `./overlay` directories. Also, a `./downloads` directory is generated to store a copy of the toolchain during the first build.
+
+## Supported Boards
+Currently images for the following devices are generated:
+* Walmart onn 4K UHD Box
+
+## Requirements
+
+- The following packages below are required to use this build script. Note that this repo uses a Dockerfile to handle most of the heavy lifting, but some system requirements still exist.
+
+`docker-ce losetup wget sudo make qemu-user-static`
+
+Note that without qemu-user-static, debootstrap will fail!
+
+## Usage
+- Just run `make`.
+- Completed builds output to `./output`
+- To cleanup and clear all builds, run `make clean`
+
+Other helpful commands:
+
+- Have a build fail and have stale mounts? `make mountclean`
+- Want to delete the download cache and do a 100% fresh build? `make distclean`
+
+## Flashing
+- Take your completed image from `./output` and extract it with gunzip
+- Flash directly to a USB drive
+  - `dd if=./debian-*.img of=/dev/mmcblk0 bs=2M conv=fdatasync`
+- Plug it into your onn 4K UHD using an OTG cable, and hold the reset button while plugging in power.
+- For future boots, you just need the USB drive plugged in during poweron. There is no more need to hold the reset button.
+
+## To Do
+* You tell me. Bug reports and PRs welcome!
+
+## Notes
+- This is a pet project that can change rapidly. Production use is not advised. Please only proceed if you know what you are doing!
---
+++ scripts/docker/build_kernel.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+
+docker_scripts_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+scripts_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+. ${scripts_path}/vars.sh
+
+# Make our temp builddir outside of the world of mounts for SPEEDS
+kernel_builddir=$(mktemp -d)
+tar -xzf ${root_path}/downloads/${kernel_filename} -C ${kernel_builddir}
+
+# Exports baby
+export PATH=${build_path}/toolchain/${toolchain_bin_path}:${PATH}
+export GCC_COLORS=auto
+export CROSS_COMPILE=${toolchain_cross_compile}
+export ARCH=arm64
+
+# Here we go
+cd ${kernel_builddir}/${kernel_filename%.tar.gz}
+
+# If we have patches, apply them
+if [[ -d ${root_path}/patches/kernel/ ]]; then
+    for file in ${root_path}/patches/kernel/*.patch; do
+        echo "Applying kernel patch ${file}"
+        patch -p1 < ${file}
+    done
+fi
+
+# Apply overlay if it exists
+if [[ -d ${root_path}/overlay/${kernel_overlay_dir}/ ]]; then
+    echo "Applying ${kernel_overlay_dir} overlay"
+    cp -R ${root_path}/overlay/${kernel_overlay_dir}/* ./
+fi
+
+# Build as normal, with our extra version set to a timestamp
+make ${kernel_config}
+make -j`getconf _NPROCESSORS_ONLN` EXTRAVERSION=-$(date +%Y%m%d-%H%M%S) bindeb-pkg dtbs
+
+# Save our config
+mkdir -p ${build_path}/kernel
+make savedefconfig
+mv defconfig ${build_path}/kernel/kernel_config
+
+# Get our kernel version (fully)
+KERNEL_VERSION=$(ls ${kernel_builddir}/linux-headers-*.deb | awk -F- '{ print $3"-"$6"-"$7 }')
+
+# Now build our deb for the dtb's
+mkdir -p ${build_path}/kernel/linux-dtbs-${KERNEL_VERSION}/boot/dtb-${KERNEL_VERSION}/amlogic
+mv ./scripts/dtb-deb/DEBIAN ${build_path}/kernel/linux-dtbs-${KERNEL_VERSION}/
+sed -i "s|KERNELVERSION|${KERNEL_VERSION}|g" ${build_path}/kernel/linux-dtbs-${KERNEL_VERSION}/DEBIAN/control
+sed -i "s|KERNELVERSION|${KERNEL_VERSION}|g" ${build_path}/kernel/linux-dtbs-${KERNEL_VERSION}/DEBIAN/conffiles
+for i in "${supported_devices[@]}"; do
+	cp arch/arm64/boot/dts/amlogic/meson-g12a-${i}.dtb ${build_path}/kernel/linux-dtbs-${KERNEL_VERSION}/boot/dtb-${KERNEL_VERSION}/amlogic
+done
+cd ${build_path}/kernel
+dpkg-deb --root-owner-group --build linux-dtbs-${KERNEL_VERSION}
+rm -rf ${build_path}/kernel/linux-dtbs-${KERNEL_VERSION}
+
+# Remove the debug kernel
+rm ${kernel_builddir}/linux-image-${KERNEL_VERSION}-dbg*.deb
+
+# Move our debs to the kernel dir
+mv ${kernel_builddir}/linux-*.deb ${build_path}/kernel
